06586395cbc2465f3d5a3e815dbd781d091d3386
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2021-02-01 19:40:20 +0500

    Update nspr-config.sh

--- .APP_CONFIG/nspr-config.sh
+++ .APP_CONFIG/nspr-config.sh
@@ -6,7 +6,7 @@ arch="tar.${ALSY_XORG_APP_CONFIG_ARCHIVE_TYPE}"
 sapp="$app-$version"
 
 if [ ! -f $sapp.$arch ]; then
-  wget https://www.libssh2.org/download/$sapp.$arch -O $sapp.$arch --no-check-certificate
+  wget https://archive.mozilla.org/pub/nspr/releases/v4.29/src/$sapp.$arch -O $sapp.$arch --no-check-certificate
 fi
 
 sed 's/@alsy.app.name/'$sapp'/g' "Makefile.am" > "Makefile"
@@ -25,6 +25,12 @@ tar -xf "$sapp"."$arch" -C ../build
 if [ $? -eq 0 ]; then
   cd ../build/$sapp
   if [ $? -eq 0 ]; then         
-    ./configure --prefix=/usr --disable-static
+    cd nspr                                                     &&
+    sed -ri 's#^(RELEASE_BINS =).*#\1#' pr/src/misc/Makefile.in &&
+    sed -i 's#$(LIBRARY) ##'            config/rules.mk         &&
+    ./configure --prefix=/usr   \
+                --with-mozilla  \
+                --with-pthreads \
+                $([ $(uname -m) = x86_64 ] && echo --enable-64bit)
   fi
 fi
