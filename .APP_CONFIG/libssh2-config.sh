aacf46e78197722497772d713bea4d6afc239e7c
semsv <semen_s84@mail.ru>
2021-02-01 14:37:04 +0000

    Mon Feb  1 02:37:04 PM UTC 2021

---
+++ .APP_CONFIG/libssh2-config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+app="${PWD##*/}"
+version="${app##*-}"
+app="${app%-*}"
+arch="tar.${ALSY_XORG_APP_CONFIG_ARCHIVE_TYPE}"
+sapp="$app-$version"
+
+if [ ! -f $sapp.$arch ]; then
+  wget https://www.libssh2.org/download/$sapp.$arch -O $sapp.$arch --no-check-certificate
+fi
+
+sed 's/@alsy.app.name/'$sapp'/g' "Makefile.am" > "Makefile"
+
+if [ -d ../build/$sapp ]; then
+ rm -rfd ../build/$sapp
+ if [ $? -eq 0 ]; then
+   echo "delete..[ ok ]"
+ else
+   exit 1
+ fi
+fi
+
+mkdir -p ../build &&
+tar -xf "$sapp"."$arch" -C ../build
+if [ $? -eq 0 ]; then
+  cd ../build/$sapp
+  if [ $? -eq 0 ]; then         
+    ./configure --prefix=/usr --disable-static
+  fi
+fi
