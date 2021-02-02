22aa98ab759504b91aa741026aa3e356b5638f45
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2021-02-02 12:17:22 +0500

    Update gdkpixbuf-config.sh

--- .APP_CONFIG/gdkpixbuf-config.sh
+++ .APP_CONFIG/gdkpixbuf-config.sh
@@ -27,7 +27,7 @@ if [ $? -eq 0 ]; then
   if [ $? -eq 0 ]; then
     rm -rfd ../pixbuf_build &&
     mkdir -p ../pixbuf_build &&
-    meson --prefix=${GTK3_PREFIX}/usr -Dupdate-mimedb=true \
+    meson --prefix=${GTK3_PREFIX}/usr \
     ../pixbuf_build
   fi
 fi
