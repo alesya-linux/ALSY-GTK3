4e5d0f164648b5c14b3980b6184f15c65fcb489a
semsv <semen_s84@mail.ru>
2021-02-02 05:32:37 +0000

    Tue Feb  2 05:32:37 AM UTC 2021

--- .APP_CONFIG/libuv-config.sh
+++ .APP_CONFIG/libuv-config.sh
@@ -6,7 +6,7 @@ arch="tar.${ALSY_XORG_APP_CONFIG_ARCHIVE_TYPE}"
 sapp="$app-$version"
 
 if [ ! -f $sapp.$arch ]; then
-  wget https://dist.libuv.org/dist/v$version/$sapp.$arch -O $sapp.$arch --no-check-certificate
+  wget https://dist.libuv.org/dist/$version/$sapp.$arch -O $sapp.$arch --no-check-certificate
 fi
 
 sed 's/@alsy.app.name/'$sapp'/g' "Makefile.am" > "Makefile"
@@ -25,6 +25,8 @@ tar -xf "$sapp"."$arch" -C ../build
 if [ $? -eq 0 ]; then
   cd ../build/$sapp
   if [ $? -eq 0 ]; then         
+ACLOCAL=""
+export ACLOCAL  
     if [ -x autogen.sh ]; then
       ./autogen.sh 
     fi &&
