ec071cecf12042bb059faf9eb55bd97bc5b1859b
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2021-02-02 10:57:24 +0500

    Update itstool-config.sh

--- .APP_CONFIG/itstool-config.sh
+++ .APP_CONFIG/itstool-config.sh
@@ -6,7 +6,7 @@ arch="tar.${ALSY_XORG_APP_CONFIG_ARCHIVE_TYPE}"
 sapp="$app-$version"
 
 if [ ! -f $sapp.$arch ]; then
-  wget https://dist.libuv.org/dist/$version/$sapp.$arch -O $sapp.$arch --no-check-certificate
+  wget http://files.itstool.org/itstool/$sapp.$arch -O $sapp.$arch --no-check-certificate
 fi
 
 sed 's/@alsy.app.name/'$sapp'/g' "Makefile.am" > "Makefile"
