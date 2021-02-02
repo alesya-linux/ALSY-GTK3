5354bbb61553d7791188d3a6726676a4f0672c47
semsv <semen_s84@mail.ru>
2021-02-02 07:31:53 +0000

    Tue Feb  2 07:31:53 AM UTC 2021

---
+++ .APP_CONFIG/cairo-config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+app="${PWD##*/}"
+version="${app##*-}"
+app="${app%-*}"
+arch="tar.${ALSY_XORG_APP_CONFIG_ARCHIVE_TYPE}"
+sapp="$app-$version"
+
+if [ ! -f $sapp.$arch ]; then
+  wget https://releases.pagure.org/$app/$sapp.$arch -O $sapp.$arch --no-check-certificate
+fi
+
+sed 's/@alsy.app.name/'$sapp'/g' "Makefile.am" > "Makefile"
+
+if [ -d ../build/$sapp ]; then
+ rm -rfd ../build/$sapp
+ if [ $? -eq 0 ]; then
+   echo "delete..[ ok ]"
+ else
+   exit 1
+ fi
+fi
+
+mkdir -p ../build &&
+tar -xf "$sapp"."$arch" -C ../build
+if [ $? -eq 0 ]; then
+  cd ../build/$sapp
+  if [ $? -eq 0 ]; then
+    LINKS="/usr/bin/links" \
+    ./configure --prefix=/usr
+  fi
+fi
